11fe65fa3a855d102e1a397875933771b8c1371d
PeakDoge <ocalakif1@gmail.com>
2026-02-01 02:44:04 +0300

    Updated the library
    Updated the library and class names. Added drive() method to MecanumDrive class to allow x and y velocity inputs. Changed setMotor to support forward and backward pwm suppRemoved unused code.

---
+++ src/MecanumDrive.cpp
@@ -0,0 +1,99 @@
+#include "MecanumDrive.h"
+
+MecanumDrive::MecanumDrive(
+	int fl_fwd_pwm, int fl_bwd_pwm,
+	int fr_fwd_pwm, int fr_bwd_pwm,
+	int rl_fwd_pwm, int rl_bwd_pwm,
+	int rr_fwd_pwm, int rr_bwd_pwm,
+	float speed_limit_percentage = 100,
+	float width = 0, float length = 0
+)
+{
+	_fl_fwd_pwm = fl_fwd_pwm;
+	_fl_bwd_pwm = fl_bwd_pwm;
+	_fr_fwd_pwm = fr_fwd_pwm;
+	_fr_bwd_pwm = fr_bwd_pwm;
+	_rl_fwd_pwm = rl_fwd_pwm;
+	_rl_bwd_pwm = rl_bwd_pwm;
+	_rr_fwd_pwm = rr_fwd_pwm;
+	_rr_bwd_pwm = rr_bwd_pwm;
+
+	pinMode(_fl_fwd_pwm, OUTPUT);
+	pinMode(_fl_bwd_pwm, OUTPUT);
+	pinMode(_fr_fwd_pwm, OUTPUT);
+	pinMode(_fr_bwd_pwm, OUTPUT);
+	pinMode(_rl_fwd_pwm, OUTPUT);
+	pinMode(_rl_bwd_pwm, OUTPUT);
+	pinMode(_rr_fwd_pwm, OUTPUT);
+	pinMode(_rr_bwd_pwm, OUTPUT);
+
+	_speed_limit_percentage = constrain(speed_limit_percentage, 0, 100);
+
+	setDimensions(width, length);
+}
+
+void MecanumDrive::setDimensions(float width, float length){
+	_length = length;
+	_width = width;
+}
+
+void MecanumDrive::setSpeedLimit(int speed_limit_percentage){
+	_speed_limit_percentage = constrain(speed_limit_percentage, 0, 100);
+}
+
+
+void MecanumDrive::drive(float x, float y, float omega){
+
+	float vmagnitude = sqrt(pow(x, 2) + pow(y, 2));
+	
+	float vx = x;
+	float vy = y;
+	float vomega = omega;
+
+	if(vmagnitude > 1){
+		vx = vx/vmagnitude;
+		vy = vy/vmagnitude;
+	}
+
+
+	// Distance term used for rotational effect
+	float LpW = _length/2 + _width/2;
+	
+	// Calculate each wheel's velocity based on mecanum kinematics
+	_v_fl = vy + vx - vomega * LpW;
+	_v_fr = vy - vx + vomega * LpW;
+	_v_rl = vy - vx - vomega * LpW;
+	_v_rr = vy + vx + vomega * LpW;
+
+	// Normalize velocities to ensure they're between -1 and 1
+	float max_v = max(max(abs(_v_fl), abs(_v_fr)), max(abs(_v_rl), abs(_v_rr)));
+	if (max_v > 1){
+		_v_fl /= max_v;
+		_v_fr /= max_v;
+		_v_rl /= max_v;
+		_v_rr /= max_v;
+	}
+
+	update();
+}
+
+// Apply the calculated velocities to each motor
+void MecanumDrive::update(){
+	setMotor(_fl_fwd_pwm, _fl_bwd_pwm, _v_fl);
+	setMotor(_fr_fwd_pwm, _fr_bwd_pwm, _v_fr);
+	setMotor(_rl_fwd_pwm, _rl_bwd_pwm, _v_rl);
+	setMotor(_rr_fwd_pwm, _rr_bwd_pwm, _v_rr);
+}
+
+// Helper function to apply a velocity to one motor
+void MecanumDrive::setMotor(int fwd_pwm, int bwd_pwm, float velocity){
+	int pwm = int(abs(velocity) * (_speed_limit_percentage/100*255));	// Scale velocity to PWM
+	if(velocity >= 0){
+		analogWrite(fwd_pwm, pwm);
+		analogWrite(bwd_pwm, 0);
+	}
+	else{
+		analogWrite(fwd_pwm, 0);
+		analogWrite(bwd_pwm, pwm);
+	}
+}
---
+++ src/MecanumDrive.h
@@ -0,0 +1,64 @@
+#ifndef MECANUM_DRIVE_H
+#define MECANUM_DRIVE_H
+
+#include <Arduino.h>
+
+class MecanumDrive {
+  public:
+    /**
+     * @param fl_fwd_pwm ccw pwm input for the front left motor in the drivetrain
+     * @param fl_bwd_pwm cw pwm input for the front left motor in the drivetrain
+     * @param fr_fwd_pwm ccw pwm input for the front right motor in the drivetrain
+     * @param fr_bwd_pwm cw pwm input for the front right motor in the drivetrain
+     * @param rl_fwd_pwm ccw pwm input for the rear left motor in the drivetrain
+     * @param rl_bwd_pwm cw pwm input for the rear left motor in the drivetrain
+     * @param rr_fwd_pwm ccw pwm input for the rear right motor in the drivetrain
+     * @param rr_bwd_pwm cw pwm input for the rear right motor in the drivetrain
+     */
+    MecanumDrive(
+		int fl_fwd_pwm, int fl_bwd_pwm,
+		int fr_fwd_pwm, int fr_bwd_pwm,
+		int rl_fwd_pwm, int rl_bwd_pwm,
+		int rr_fwd_pwm, int rr_bwd_pwm,
+		float speed_limit_percentage = 100,
+		float width = 0, float length = 0
+	);
+
+    // Set the robot's physical dimensions in meters
+    void setDimensions(float width, float length);
+
+    // Set the speed limit for PWM output as percentage (from 0% to 100%)
+    void setSpeedLimit(int speed_limit_percentage);
+
+    /**
+     * @param vx right positive velocity multiplier
+     * @param vy forward positive velocity multiplier
+     */
+    void drive(float vx, float vy, float vomega)
+
+    
+
+  private:
+    // Motor pin assignments
+    int _fl_fwd_pwm, _fl_bwd_pwm;
+    int _fr_fwd_pwm, _fr_bwd_pwm;
+    int _rl_fwd_pwm, _rl_bwd_pwm;
+    int _rr_fwd_pwm, _rr_bwd_pwm;
+
+    // Robot dimensions in meters
+    float _length, _width;
+
+    // Speed limit for PWM output as percentage (from 0% to 100%)
+    int _speed_limit_percentage;
+
+    // Individual motor velocity values (normalized)
+    float _v_fl, _v_fr, _v_rl, _v_rr;
+
+	// Compute and apply motor outputs
+    void update();
+
+	// Set motor velocity with forwards and backwards pwm
+    void setMotor(int fwd_PWM, int bwd_PWM, float velocity);
+};
+
+#endif
